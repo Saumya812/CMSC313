d3a67739856b5e491d2456e3cafdd46e73d7f9ec
Saumya  Brahmbhatt <139834472+Saumya812@users.noreply.github.com>
2025-10-30 17:56:18 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-  printf("Hello teach. My name is Saumya Brahmbhatt and I'm in your CMPE313 class.");
+  printf("Hello prof. Kidd. My name is Saumya Brahmbhatt and I'm in your CMSC313 class.");
   return 0; 
 
 }
